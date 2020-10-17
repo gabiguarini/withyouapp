f878e16ef3530beed90d068143b46a42e571c94d
Gabriela Guarini <52461052+gabiguarini@users.noreply.github.com>
2020-10-17 03:21:48 -0300

    fixes
    conflicts

--- App.tsx
+++ App.tsx
@@ -1,37 +1,22 @@
-<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-native-paper';
+import App from './src';
+import { theme } from './src/core/theme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
-}
-=======
-import React from 'react';
-import { Provider } from 'react-native-paper';
-import App from './src';
-import { theme } from './src/core/theme';
-
 const Main = () => (
   <Provider theme={theme}>
-    <App />
-  </Provider>
+        <App />
+        
+    </Provider>
+    
 );
 
+
+
 export default Main;
->>>>>>> c1a7bd9f6bc339f0fc7b4d4e7ace85662a15449c
+
